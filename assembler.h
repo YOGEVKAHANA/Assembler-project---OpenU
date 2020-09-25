e5db87db9eb0497001d4b2aa2e1cb4a35706a7fa
YOGEVKAHANA <71885363+YOGEVKAHANA@users.noreply.github.com>
2020-09-25 16:50:53 +0300

    Create assembler.h

---
+++ assembler.h
@@ -0,0 +1,13 @@
+
+/*
+ (c) Yogev Kahana 2020c
+ */
+ 
+ #include "definitions.h"
+#define MAX_LINE_LEN     80+1 /* Maximum  line + 1 for /n char */
+extern int ParsAssemblyFiles ( char *fileName);
+ 
+ 
+/*
+ (c) Yogev Kahana 2020c
+ */
