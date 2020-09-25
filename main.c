f0e7660f91e0a00a5c5e536ca23794490a7f6d4b
YOGEVKAHANA <71885363+YOGEVKAHANA@users.noreply.github.com>
2020-09-25 16:56:13 +0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,20 @@
+/*
+ (c) Yogev Kahana 2020c
+ */
+ /*#include "main.h"*/
+#include <stdio.h>
+#include "main_loop.h"
+
+/*argc- num' of files that need to be proccessd*/
+/*argv pointer to argc */
+/*will return 1*/
+int  main(int argc, char *argv[])
+{
+   ProcessAssemblyFiles(argc-1, argv+1);
+
+   return 1;
+
+}
+ /*
+ (c) Yogev Kahana 2020c
+ */
