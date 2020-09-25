e3c84e2a418e9436d147ce109a52c6033b9dcbe8
YOGEVKAHANA <71885363+YOGEVKAHANA@users.noreply.github.com>
2020-09-25 16:52:17 +0300

    Create definitions.c

---
+++ definitions.c
@@ -0,0 +1,35 @@
+ /*
+ (c) Yogev Kahana 2020c
+ */
+ 
+ #include <stdio.h>
+#include <errno.h>
+#include "definitions.h"
+
+/*check if a file exist according to file name*/
+BOOL IsFileExist( char *fileName)
+{
+FILE *file;
+BOOL fileExist=FALSE;
+errno = 0;
+ 
+    file = fopen(fileName,"r");
+    
+	if( file != NULL)
+	{
+       fclose(file);
+       fileExist = TRUE;
+	}
+	
+    return fileExist;
+}
+
+BOOL DeleteFile(char *fileName)
+{
+
+	return remove(fileName)? TRUE : FALSE;/*wiil return T or F*/
+}
+
+  /*
+ (c) Yogev Kahana 2020c
+ */
