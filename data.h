9b98c0bbe03efaffd234c197d236b6340ba67d9b
YOGEVKAHANA <71885363+YOGEVKAHANA@users.noreply.github.com>
2020-09-25 16:51:54 +0300

    Create data.h

---
+++ data.h
@@ -0,0 +1,24 @@
+
+ /*
+ (c) Yogev Kahana 2020c
+ */
+ 
+ /*struct a label according to rules of labels and syntax*/
+struct  LabelDefintion {
+ char *lableName;
+ enum lableTypes lableType;
+ int  lableValue;
+ int  lableLength;
+};
+
+extern void ResetLablesTable();
+/*function decleration*/
+extern BOOL IsLableExist( char *lable, char lableLength, enum lableTypes *lableType, int  *lableValue);
+extern BOOL IsLableTypeCodeOrExternExist( char *lable, char lableLength, enum lableTypes *lableType, int  *lableValue);
+extern BOOL EnterLable( char *lable, int lableLength, enum lableTypes lableType, int  lableValue);
+extern int  GetSymbolTableData(struct LabelDefintion **labelsTable);
+
+ 
+ /*
+ (c) Yogev Kahana 2020c
+ */
